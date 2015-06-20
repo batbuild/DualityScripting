3aa8a1518e5b244ba65226ca46a18789921c4510
Andrea <roundcrisis@gmail.com>
2015-06-20 18:45:30 +0100

    Build script for android duality scripting plugin

--- Build.bat
+++ Build.bat
@@ -1,4 +1,6 @@
 @echo off
 cls
-".nuget\NuGet.exe" "Install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion"
-"packages\FAKE\tools\Fake.exe" CompleteBuild.fsx
+if not exist packages\FAKE\tools\Fake.exe (
+	".nuget\NuGet.exe" "Install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion"
+)
+"packages\FAKE\tools\Fake.exe" CompleteBuild.fsx %*
